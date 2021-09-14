cff43c298ba10a8f5870d75136a8cf1f28db289f
Talles Corradini <tallescorradini@gmail.com>
2021-09-14 17:48:48 -0300

    feat: style header

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import NavigationBar from './components/NavigationBar'
+import * as styles from './index.module.css'
 
 const IndexPage = () => {
   return (
     <div>
       <header>
-        <a href="#home">talles corradini</a>
+        <a className={styles.logo} href="#home">
+          talles corradini
+        </a>
         <NavigationBar />
       </header>
       <main>
