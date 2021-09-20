42443eaa8876dd7b5a3f464acbdc38788c725691
Talles Corradini <tallescorradini@gmail.com>
2021-09-20 10:20:42 -0300

    feat: serve pdf when clicking on download CV button

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 import NavigationBar from './components/NavigationBar'
 import * as styles from './index.module.css'
+import resumeFile from './downloads/Talles_Corradini_Resume.pdf'
+
+// [x] TODO feat: add project and download CV buttons
+// [x] TODO feat: serve pdf when clicking on download CV button
+// [ ] TODO refactor: create HomeSection component
+// [ ] TODO feat (NavigationBar): add navigation for smaller devices
 
 const IndexPage = () => {
   return (
@@ -17,8 +23,24 @@ const IndexPage = () => {
       </header>
       <main>
         <section id="home" data-testid="home">
-          I'm a Front End Web Developer
-          <a href="#projects">See projects</a>
+          <h1>Hello! I'm Talles</h1>
+          <h2>
+            I'm a Front End Web Developer ready to code modern and responsive
+            web pages
+          </h2>
+          <div className={styles.buttonWrapper}>
+            <a href="#projects" className={styles.buttonFilled}>
+              See projects
+            </a>
+            <a
+              href={resumeFile}
+              className={styles.buttonOutlined}
+              target="_blank"
+              download
+            >
+              Download CV
+            </a>
+          </div>
         </section>
 
         <section id="projects" data-testid="projects">
