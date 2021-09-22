1e7720f7e5b14f894eb31f1e46e301e09e15acab
Talles Corradini <tallescorradini@gmail.com>
2021-09-22 18:57:26 -0300

    feat (ContactSection): add contact section

---
+++ src/pages/components/ContactSection/ContactSection.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import * as styles from './ContactSection.module.css'
+
+const ContactSection = () => {
+  return (
+    <section id="contact" data-testid="contact">
+      <h2 className={styles.title}>Contact</h2>
+
+      <div className={styles.container}>
+        <form
+          className={styles.form}
+          name="contact"
+          method="POST"
+          data-netlify="true"
+        >
+          <div className={styles.field}>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              required
+              placeholder=" "
+              className={styles.input}
+            ></input>
+            <label htmlFor="name">Name</label>
+          </div>
+          <div className={styles.field}>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              required
+              placeholder=" "
+              className={styles.input}
+            ></input>
+            <label htmlFor="email">E-mail</label>
+          </div>
+          <div className={styles.field}>
+            <textarea
+              id="message"
+              name="message"
+              required
+              placeholder=" "
+              className={styles.input}
+              rows="4"
+            ></textarea>
+            <label htmlFor="message">Message</label>
+          </div>
+
+          <button type="submit" className={styles.buttonFilled}>
+            Send
+          </button>
+        </form>
+        <p>You can also find me on:</p>
+      </div>
+    </section>
+  )
+}
+
+export default ContactSection
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import NavigationBar from './components/NavigationBar/NavigationBar'
 import HomeSection from './components/HomeSection/HomeSection'
 import * as styles from './index.module.css'
+import ContactSection from './components/ContactSection/ContactSection'
 
 // [x] TODO feat: add project and download CV buttons
 // [x] TODO feat: serve pdf when clicking on download CV button
@@ -14,6 +15,10 @@ import * as styles from './index.module.css'
 // [x] TODO fix: show navBar on large devices even when menuButton is open
 // [x] TODO fix: make header always on top
 // [x] TODO feat: add clickable arrow to home page
+// [ ] TODO feat: add form to reach out
+// [ ] TODO fix: header should not overlap section titles
+// [ ] TODO fix (HomeSection): center arrowIcon
+// [ ] TODO fix (NavigationBar): make navigation link unclickable when menu is inactive
 
 const IndexPage = () => {
   return (
@@ -34,9 +39,7 @@ const IndexPage = () => {
           Projects
         </section>
 
-        <section id="contact" data-testid="contact">
-          Contact
-        </section>
+        <ContactSection />
       </main>
     </div>
   )
