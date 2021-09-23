4c4f31bac6d87a208b90051312fabb356e43f6a1
Talles Corradini <tallescorradini@gmail.com>
2021-09-23 11:43:11 -0300

    netlify test AJAX

--- src/pages/components/ContactSection/ContactSection.jsx
+++ src/pages/components/ContactSection/ContactSection.jsx
@@ -3,17 +3,31 @@ import React from 'react'
 import * as styles from './ContactSection.module.css'
 
 const ContactSection = () => {
+  const handleSubmit = event => {
+    event.preventDefault()
+    let myForm = document.getElementById('contact-form')
+    let formData = new FormData(myForm)
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log('Form successfully submitted'))
+      .catch(error => alert(error))
+  }
+
   return (
     <section id="contact" data-testid="contact">
       <h2 className={styles.title}>Contact</h2>
 
       <div className={styles.container}>
         <form
-          className={styles.form}
-          method="post"
+          id="contact-form"
+          onSubmit={handleSubmit}
           netlify-honeypot="bot-field"
           data-netlify="true"
           name="contact"
+          className={styles.form}
         >
           <input type="hidden" name="bot-field" />
           <input type="hidden" name="form-name" value="contact" />
