1731e4871e9f6c16be21469e9dc4987a1eb336b3
Talles Corradini <tallescorradini@gmail.com>
2021-09-29 11:45:57 -0300

    feat: add project section for medium screens

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,7 +12,7 @@ import ProjectsSection from './components/ProjectsSection/ProjectsSection'
 // [x] TODO feat: show submission message without leaving the page
 // [x] TODO feat: link to social media
 // [x] TODO fix: align contact section on large screens
-// [ ] TODO feat: add projects section (1/3)
+// [ ] TODO feat: add projects section (2/3)
 // [ ] TODO fix: header should not overlap section titles
 // [ ] TODO fix (HomeSection): center arrowIcon inside button
 // [ ] TODO fix (NavigationBar): make navigation link unclickable when menu is inactive
