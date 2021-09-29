7daf82bfe4a0668cd7b4aebc4e0712f136e1ce68
Talles Corradini <tallescorradini@gmail.com>
2021-09-29 16:01:46 -0300

    fix: navigate to section without top bar overlap

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,16 +6,10 @@ import * as styles from './index.module.css'
 import ContactSection from './components/ContactSection/ContactSection'
 import ProjectsSection from './components/ProjectsSection/ProjectsSection'
 
-// [x] TODO feat: add clickable arrow to home page
-// [x] TODO feat: add form to reach out
-// [x] TODO refactor (ContactSection): clean up css
-// [x] TODO feat: show submission message without leaving the page
-// [x] TODO feat: link to social media
-// [x] TODO fix: align contact section on large screens
-// [x] TODO feat: add projects section (3/3)
-// [ ] TODO fix: header should not overlap section titles
+// [x] TODO fix: header should not overlap section titles
 // [ ] TODO fix (HomeSection): center arrowIcon inside button
 // [ ] TODO fix (NavigationBar): make navigation link unclickable when menu is inactive
+// [ ] TODO fix: (HomeSection): set smaller font-size for small screens
 
 // [ ] TODO feat: add form validation
 // [ ] TODO feat: animate form submission message (using CSSTransition)
